373fbbe0ab1322d9895295daab2091398b8f252d
Vinicius Bussolo <vinicius.buss13@gmail.com>
2025-02-11 09:41:54 -0300

    feat: criado a section de planos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
               <ul className="flex items-center gap-6">
                 <li>
                   <a
-                    href="#funcionamento"
+                    href="#"
                     className="text-black text-sm hover:text-gray-600 transition-colors"
                   >
                     Funcionamento
@@ -43,7 +43,7 @@ export default function Home() {
             <h1 className="font-bold text-6xl text-black text-center">
               Simplifique Seus Estudos
             </h1>
-            <p className="text-center 6B7280 text-xl max-w-2xl">
+            <p className="text-center text-xl max-w-2xl text-gray-500">
               Deixe que nós fazemos a curadoria para você. Assine nossa
               plataforma e receba todos os meses um ebook novo de programação.
             </p>
@@ -116,6 +116,87 @@ export default function Home() {
               </div>
             </div>
           </div>
+        </div>
+      </section>
+
+      <section className="w-full flex flex-col justify-center items-center mt-52 px-3 gap-[4.625rem]">
+        <div className="w-full flex flex-col items-center gap-6">
+          <h1 className="font-bold text-6xl text-black text-center">
+            Preço Simples e Transparente
+          </h1>
+          <p className="text-center text-xl max-w-2xl text-gray-500">
+            Pra que inúmeros planos quando nós sabemos exatamente o que é melhor
+            para você? Assine o nosso plano mensal{" "}
+            <span className="underline">Pro Premium VIP</span> e garanta
+            mensalmente um ebook novo de programação. E por menos de um café por
+            dia.
+          </p>
+        </div>
+
+        <div className="max-w-[26.125rem] w-full flex flex-col items-start gap-11 pl-10 pr-12">
+          <div className="space-y-2">
+            <h3 className="text-2xl font-semibold text-black">
+              Plano Pro Premium VIP
+            </h3>
+
+            <p className="text-sm text-gray-500">
+              Tudo de você precisa para seus estudos
+            </p>
+          </div>
+
+          <div className="space-y-4">
+            <p className="text-xl text-gray-500">
+              <span className="text-4xl font-semibold text-black">R$29</span>
+              /mês
+            </p>
+            <div className="space-y-3">
+              <div className="flex items-center gap-5">
+                <Image
+                  src="./vector.svg"
+                  alt="Imagem de um vetor"
+                  width={14}
+                  height={10}
+                />
+                <p className="text-sm text-gray-500">1 ebook por mês</p>
+              </div>
+
+              <div className="flex items-center gap-5">
+                <Image
+                  src="./vector.svg"
+                  alt="Imagem de um vetor"
+                  width={14}
+                  height={10}
+                />
+                <p className="text-sm text-gray-500">Curadoria especial</p>
+              </div>
+
+              <div className="flex items-center gap-5">
+                <Image
+                  src="./vector.svg"
+                  alt="Imagem de um vetor"
+                  width={14}
+                  height={10}
+                />
+                <p className="text-sm text-gray-500">Acesso ilimitado</p>
+              </div>
+
+              <div className="flex items-center gap-5">
+                <Image
+                  src="./vector.svg"
+                  alt="Imagem de um vetor"
+                  width={14}
+                  height={10}
+                />
+                <p className="text-sm text-gray-500">
+                  Cancele a qualquer momento
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <button className="w-full h-10 bg-black text-white rounded-md text-sm">
+            Assine Agora
+          </button>
         </div>
       </section>
     </>

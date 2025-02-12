292287d9ec7a9c91c7100848e357e3c0b0035533
Vinicius Bussolo <vinicius.buss13@gmail.com>
2025-02-12 10:20:47 -0300

    feat: criado a pagina de registrar

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Login() {
+export default function Register() {
   return (
     <main className="w-full flex justify-center items-center h-screen">
       <div className="max-w-[22rem] w-full flex flex-col items-center ">
@@ -9,14 +9,26 @@ export default function Login() {
 
         <div className="w-full">
           <div className="mt-11">
-            <h1 className="font-semibold text-xl text-black">Boas Vindas</h1>
+            <h1 className="font-semibold text-xl text-black">Cadastre-se</h1>
             <p className="text-sm text-gray-500">
-              Faça seu login com email e senha
+              Faça seu cadastro gratuitamente
             </p>
           </div>
 
           <form className="mt-11 w-full space-y-7 ">
             <div className="space-y-5">
+              <div className="flex flex-col gap-2">
+                <label htmlFor="name" className="font-bold text-xs">
+                  Nome
+                </label>
+                <input
+                  type="name"
+                  name="name"
+                  placeholder="Fulano de Tal"
+                  className="text-sm p-1"
+                />
+              </div>
+
               <div className="flex flex-col gap-2">
                 <label htmlFor="email" className="font-bold text-xs">
                   Email
@@ -43,7 +55,7 @@ export default function Login() {
             </div>
 
             <button className="w-full h-10 bg-black text-white rounded-md text-sm hover:bg-gray-800 transition-colors">
-              Login
+              Registrar
             </button>
 
             <p className="font-light text-[0.625rem] text-gray-500 text-center mt-6">
@@ -54,12 +66,12 @@ export default function Login() {
         </div>
 
         <p className="font-light text-[0.625rem] text-gray-500 mt-11">
-          Não possui cadastro?{" "}
+          Já possui cadastro?{" "}
           <Link
-            href={"/register"}
+            href={"/login"}
             className="text-[#5E6063] font-bold hover:text-gray-800"
           >
-            Registre-se
+            Faça o Login
           </Link>
         </p>
       </div>
